3118a7744acaf040b40a3d6186658a3629ebb576
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-09-13 14:55:56 -0700

    Initial definition of battlefield class methods

--- battlefield.py
+++ battlefield.py
@@ -1,4 +1,25 @@
+from herd import Herd
+from fleet import Fleet
+from dinosaur import Dinosaur
+from robot import Robot
+
+
+
+
+
 class Battlefield:
 
     def __init__(self):
+        pass
+
+    def run_game(self):
+        pass
+    
+    def display_welcome(self):
+        pass
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
         pass
