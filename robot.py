de355926c1f4352f4060e369e70de4eb61193473
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-09-13 12:08:39 -0700

    Initial robot class definition of methods

--- robot.py
+++ robot.py
@@ -1,12 +1,19 @@
+from weapon import Weapon
+import random
+
+sword = Weapon("Sword", 75)
+rocketLauncher = Weapon("Rocket Launcher", 250)
+laser = Weapon("Laser", 150)
 class Robot:
-
-    from weapon import Weapon
-
-    sword = Weapon("Sword", 75)
-    rocketLauncher = Weapon("Rocket Launcher", 250)
-    laser = Weapon("Laser", 150)
 
     def __init__(self, robotName):
         self.name = robotName
         self.health = 750
-        self.active_weapon = ""
+        self.weapons = [sword, rocketLauncher, laser]
+        self.active_weapon = sword
+
+    def chooseWeapon (self):
+        pass
+
+    def attack (self, dinosaur):
+        pass
