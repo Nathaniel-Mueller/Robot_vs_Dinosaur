fc6e1989c7fe0ed8543edac485b8c16f7dbab953
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-09-13 13:57:48 -0700

    Initial dinosaur class definition of methods

--- dinosaur.py
+++ dinosaur.py
@@ -1,4 +1,9 @@
 class Dinosaur:
 
-    def __init__(self) -> None:
+    def __init__(self, dinoType, attackStrength):
+        self.name = dinoType
+        self.attack_power = attackStrength
+        self.health = 500
+    
+    def attack(self, robot):
         pass
