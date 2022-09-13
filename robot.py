f239e79856a2abc3ab6369d1f1363710ba773f60
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-09-13 14:07:07 -0700

    Defined robot class' weapon selection method

--- robot.py
+++ robot.py
@@ -1,5 +1,5 @@
 from weapon import Weapon
-import random
+
 
 sword = Weapon("Sword", 75)
 rocketLauncher = Weapon("Rocket Launcher", 250)
@@ -12,8 +12,16 @@ class Robot:
         self.weapons = [sword, rocketLauncher, laser]
         self.active_weapon = sword
 
-    def chooseWeapon (self):
-        pass
+    def chooseWeapon (self, newWeapon):
+        if newWeapon == "sword":
+            self.active_weapon = self.weapons[0]
+            print (f"{self.active_weapon} selected!")
+        elif newWeapon == "rocket launcher":
+            self.active_weapon = self.weapons[1]
+            print (f"{self.active_weapon} selected!")
+        elif newWeapon == "laser":
+            self.active_weapon = self.weapons[2]
+            print(f"{self.active_weapon} selected!")
 
     def attack (self, dinosaur):
         pass
